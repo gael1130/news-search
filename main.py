c2186134bf86da3b0bc334f007d6013755b42c6d
kalel <kalel1130@Gael-G15>
2024-09-30 18:20:08 +0200

    added interactivity

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from utils.harmonize_classify_verify import create_safe_filename
 from datetime import datetime
 
 def main():
-    search_terms = ["Cérélia"]
+    # search_terms = ["Cérélia"]
+    # Prompt the user to enter a search term
+    search_term = input("Enter the search term: ")
+    search_terms = [search_term]
+
     results = perform_searches_for_all_periods(search_terms)
     safe_name = create_safe_filename(search_terms[0])
     csv_filename = f"results/{safe_name}_{datetime.now().strftime('%Y_%m_%d_%Hh%M')}.csv"
     save_results_to_csv(results, csv_filename)
+    print(f"*** Results saved to {csv_filename} ***")
 
 if __name__ == "__main__":
     main()
